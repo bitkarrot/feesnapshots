7a602f854a6e6f35eb56d7e849d9a87c9c5f50fe
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-05-01 22:11:20 -0700

    add logging

--- fee_screenshots.py
+++ fee_screenshots.py
@@ -2,9 +2,22 @@ import subprocess
 import aiocron
 import asyncio
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger('feelogger').setLevel(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+path  = "./"
+############# logfile ############
+level = logging.INFO
+logger.setLevel(level)
+log_path = path + "logfile"
+###################################
 
 joecommand = "pageres https://jochen-hoenicke.de/queue/#BTC,24h,fee --filename=images/johoe_24h --selector=\'canvas.flot-overlay\'"
 wtfcommand = "pageres https://whatthefee.io/ --filename=images/wtf --selector=\'table.FeeTable\'"
+
 
 #### cron job ####
 #@aiocron.crontab('0 0/12 * * *')
@@ -12,21 +25,21 @@ wtfcommand = "pageres https://whatthefee.io/ --filename=images/wtf --selector=\'
 async def attime():
     try:
         homedir = os.getcwd()
-        print("attempting to remove old versions")
+        logger.info("attempting to remove old versions")
         run1 = subprocess.run(["rm", homedir + "/images/wtf.png"])
         run2 = subprocess.run(["rm", homedir + "/images/johoe_24h.png"])
-        print(f'wtf: {run1} , johoe: {run2}')
-        
-        print("starting cron to fetch screenshots")
+        logger.info(f'wtf: {run1} , johoe: {run2}')
+
+        logger.info("starting cron to fetch screenshots")
         res1 = subprocess.call(joecommand, shell = True)
         res2 = subprocess.call(wtfcommand, shell = True)
 
         if res1 == 0:
-            print("ok geting joehoe chart")
+            logger.info("ok geting joehoe chart")
         if res2 == 0:
-            print("ok getting wtf chart")            
+            logger.info("ok getting wtf chart")            
     except Exception as e:
-        print(e)
+        logger.info(e)
 
 
 
