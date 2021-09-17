4170fe01cd2ed951df7d0bc2e4b278ac38065663
bitkarrot <christina.c@bitcoin.org.hk>
2021-09-17 00:27:25 +0000

    rearrange

--- fee_screenshots.py
+++ fee_screenshots.py
@@ -3,6 +3,7 @@ import aiocron
 import asyncio
 import os
 import logging
+from os.path import exists
 
 logging.basicConfig(filename='feesnaps.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger('feelogger').setLevel(level=logging.WARNING)
@@ -12,28 +13,37 @@ joecommand = "pageres https://jochen-hoenicke.de/queue/#BTC,24h,fee --filename=i
 wtfcommand = "pageres https://whatthefee.io/ --filename=images/wtf --selector=\'table.FeeTable\'"
 
 #### cron job ####
-#@aiocron.crontab('0 0/12 * * *')
+#@aiocron.crontab('0 * * * *')
 @aiocron.crontab('* * * * *')
 async def attime():
     try:
         homedir = os.getcwd()
         logger.info("attempting to remove old versions")
-        run1 = subprocess.run(["rm", homedir + "/images/wtf.png"])
-        run2 = subprocess.run(["rm", homedir + "/images/johoe_24h.png"])
-        logger.info(f'wtf: {run1} , johoe: {run2}')
+        wtf = homedir + "/images/wtf.png"
+        wtf_file_exists = exists(wtf)
+        johoe = homedir + "/images/johoe_24h.png"
+        johoe_file_exists = exists(johoe)
 
-        logger.info("starting cron to fetch screenshots")
+        if wtf_file_exists:                    
+            run1 = subprocess.run(["rm", wtf])
+            logger.info(f'wtf: {run1}')
+        if johoe_file_exists:            
+            run2 = subprocess.run(["rm", johoe])        
+            logger.info(f'johoe: {run2}')
+
+        logger.info("starting job to fetch screenshots")
         res1 = subprocess.call(joecommand, shell = True)
         res2 = subprocess.call(wtfcommand, shell = True)
 
         if res1 == 0:
             logger.info("ok geting joehoe chart")
         if res2 == 0:
-            logger.info("ok getting wtf chart")            
+            logger.info("ok getting wtf chart")
+            
     except Exception as e:
-        logger.info(e)
+            logger.info(e)
 
 
-
+            
 attime.start()
 asyncio.get_event_loop().run_forever()
