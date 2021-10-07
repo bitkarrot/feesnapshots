3a112e6f327969bd7a715191f790fa8571fc215a
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-10-06 18:01:24 -0700

    add sats snapshot

--- getimages.js
+++ getimages.js
@@ -1,11 +1,12 @@
 const Pageres = require('pageres');
 
-(async () => {
-        await new Pageres({delay: 2})
-        .src('https://jochen-hoenicke.de/queue/#BTC,24h,fee', ['1280x1024'], {filename: 'johoe_24h', crop: true})
-        .src('https://whatthefee.io/', ['1024x768'], {selector: 'table.FeeTable', filename: 'wtf', crop: true})
-                .dest('./images')
-                .run();
+(async() => {
+    await new Pageres({ delay: 2 })
+        .src('https://jochen-hoenicke.de/queue/#BTC,24h,fee', ['1280x1024'], { filename: 'johoe_24h', crop: true })
+        .src('https://whatthefee.io/', ['1024x768'], { selector: 'table.FeeTable', filename: 'wtf', crop: true })
+        .src('https://sats.bitcoin.org.hk/en', ['550x900'])
+        .dest('./images')
+        .run();
 
-        console.log('Finished generating screenshots!');
+    console.log('Finished generating screenshots!');
 })();
